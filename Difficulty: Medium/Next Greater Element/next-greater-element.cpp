a5be9fd2ce289dc6508ba1fd0b451ee549435116
Himanshu Prajapati <himanshuprajapati15072003@gmail.com>
2026-06-02 22:36:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int>ans(n,-1);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            
+            // check if the stack is empty or not and also the greater element;
+            while(!st.empty() && st.top()<=arr[i])st.pop();
+            if(!st.empty())ans[i]=st.top();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
