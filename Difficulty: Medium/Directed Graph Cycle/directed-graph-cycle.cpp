3bd3f1766273c25189d7a00930302a7ede4be864
Himanshu Prajapati <himanshuprajapati15072003@gmail.com>
2026-06-24 06:41:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,32 +1,36 @@
 class Solution {
   public:
-  bool dfs(int source,vector<vector<int>>&adj,vector<bool>&visited, vector<bool>&recursive_path){
-      visited[source]=true;
-      recursive_path[source]=true;
-      for(auto child:adj[source]){
-          if(!visited[child]){
-              if(dfs(child,adj,visited,recursive_path))return true;
-          }else if(recursive_path[child]){
-              return true;
-          }
+  bool dfs(int source,vector<vector<int>>&adj,vector<int>&colours){
+  // in thsi 0--> means not visited
+  // 1 --> means currently in stack;
+  // 2 ---> measn visited;
+  colours[source]=1;
+  for(auto &child:adj[source]){
+      if(colours[child]==0){
+         if (dfs(child,adj,colours)) return true;
       }
-     recursive_path[source] = false;
-    return false;
+      if(colours[child]==1)return true ; // this refers tht out dfs is currently in stack;
+      
+  }
+  colours[source]=2;
+  return false;
   }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         int n=V;
         vector<vector<int>>adj(n);
-        vector<bool>visited(n,false);
-        vector<bool>recursive_path(n,false);
+        // vector<bool>visited(n,false);
+        // vector<bool>recursive_path(n,false);
+        
+        vector<int>colours(n);
         for(auto &a:edges){
             int u=a[0];
             int v=a[1];
             adj[u].push_back(v);
         }
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-              if(dfs(i,adj,visited,recursive_path))return true;
+            if(colours[i]==0){
+              if(dfs(i,adj,colours))return true;
         }
         }
         return false;
